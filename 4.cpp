da264cadd16cbaa55295c69d6b1226f481ec73db
LiuY <lyssfxz@126.com>
2017-11-29 14:37:46 +0800

    Median of Two Sorted Arrays

---
+++ 4.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int C[m+n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while(i < m && j < n)
+        {
+            if(nums1[i] <= nums2[j])
+            {
+                C[k++] = nums1[i++];
+            }
+            else
+            {
+                C[k++] = nums2[j++];
+            }
+        }
+        
+        if(i == m)
+        {
+            while( j < n )
+            {
+                C[k++] = nums2[j++];
+            }
+        }
+        else
+        {
+            while( i < m )
+            {
+                C[k++] = nums1[i++];
+            }
+        }
+        
+        k = n + m;
+        if( k % 2 == 0)
+        {
+            return (C[k/2] + C[k/2 - 1])/2.0;
+        }
+        else
+        {
+            return C[k/2];
+        }
+    }
+};
